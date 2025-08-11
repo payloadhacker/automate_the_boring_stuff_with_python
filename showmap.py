b52d8d9940fab53da8da76483f68c18d5f842b33
payloadhacker <72625302+payloadhacker@users.noreply.github.com>
2025-08-11 14:39:56 +0100

    Add backup, clipboard recording, fish tank simulation, map opening, and snowstorm animation scripts

---
+++ showmap.py
@@ -0,0 +1,9 @@
+import webbrowser, sys, pyperclip
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard.
+    address = pyperclip.paste()
+ # Open the web browser.
+webbrowser.open('https://www.openstreetmap.org/search?query=' + address)
